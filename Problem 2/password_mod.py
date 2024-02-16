028de78e9c0fa4939a2a28529088b07402b24cae
Mason Anderson <mta0627@utulsa.edu>
2024-02-16 15:54:46 -0600

    Wrote the code for problem 2

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -1,14 +1,15 @@
 """
 Complete the following python code to print the password entered by the user with the modifications described in the readme
 
-Name:
-Lab Time:
+Name: Mason Anderson
+Lab Time: 2/16/24 3:54 PM
 """
 
 def password_mod():
     word = input()
-    password = ''
     # Type your code here.
+    password = word.replace('i','1').replace('a','@').replace('m','M').replace('B','8').replace('s','$') + '!'
+    print(password)
 
 if __name__ == "__main__":
     password_mod()
