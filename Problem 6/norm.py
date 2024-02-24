9411a584e07d55efe372efc0374d1da4318e7447
Mason Anderson <mta0627@utulsa.edu>
2024-02-23 20:03:17 -0600

    Wrote code for problem 6

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -1,13 +1,26 @@
 """
 Complete the following python code to take in a list of values from the user and then normalize them
 
-Name:
-Lab Time:
+Name: Mason Anderson
+Lab Time: 8:02 PM 2/23/24
 """
 
 def norm():
     # Write your code here
-    pass
+    
+    num_of_data_values = int(input())
+    for i in range(num_of_data_values):
+        
+        if i == 0:
+            data_set = [float(input())]
+        else:
+            data_set.append(float(input()))
+    
+    max_value = max(data_set)
+    adjusted_set = [num / max_value for num in data_set]
+    
+    for num in iter(adjusted_set):
+        print (f'{num:.2f}')
 
 if __name__ == "__main__":
     norm()
