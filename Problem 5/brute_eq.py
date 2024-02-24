3d1b882d0499693a2310fdf7d274a5dbc49c5fd1
Mason Anderson <mta0627@utulsa.edu>
2024-02-23 19:33:42 -0600

    Wrote code for problem 5

--- Problem 5/brute_eq.py
+++ Problem 5/brute_eq.py
@@ -3,8 +3,8 @@ Complete the python code to find the solution to the system of linear equations 
 The equations are of the form ax + by = c and dx + ey = f. The solution should be printed in the form x = # and y = #. 
 If there is no solution, print "There is no solution".
 
-Name:
-Lab Time:
+Name: Mason Anderson
+Lab Time: 7:33 PM 2/23/24
 """
 
 def brute_eq():
@@ -19,6 +19,20 @@ def brute_eq():
     f = int(input())
 
     # YOUR CODE HERE
-    
+    solution = False
+   
+    for x in range (-10,11):
+        for y in range (-10,11):
+            if a*x + b*y == c and d*x + e*y == f:
+                print (f'x = {x} , y = {y}')
+                solution = True
+            if solution:
+                break
+        if solution:
+            break
+
+    if solution != True:
+        print ('There is no solution')
+
 if __name__ == "__main__":
     brute_eq()
